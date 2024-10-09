c338a1a7fc627d31f0b871f1fb10fd2f8919c048
Ricardo Guerra <RikkuG491@gmail.com>
2024-10-09 16:15:25 -0400

    Added solution code

--- lib/animal.py
+++ lib/animal.py
@@ -1,3 +1,33 @@
 import ipdb
 
-# write your code here
+# Bonus Deliverable # 1 solution code
+class Animal:
+
+    # Bonus Deliverable # 1 solution code
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    # Bonus Deliverable # 2 solution code
+    def make_animal_sound(self):
+        print("Animal Sound!")
+
+# Bonus Deliverable # 3 solution code
+class Dog(Animal):
+
+    # Bonus Deliverable # 4 solution code
+    def __init__(self, name, age, bark_volume=1, obedience_level=3):
+        super().__init__(name, age)
+        self.bark_volume = bark_volume
+        self.obedience_level = obedience_level
+
+    # Bonus Deliverable # 5 solution code
+    def make_animal_sound(self):
+        print(f"Bark{'!' * self.bark_volume}")
+
+# Bonus Deliverable # 6 solution code
+class Cat(Animal):
+    
+    # Bonus Deliverable # 7 solution code
+    def make_animal_sound(self):
+        print("Meow!")
--- lib/car.py
+++ lib/car.py
@@ -1,3 +1,60 @@
 import ipdb
 
-# write your code here
+# Deliverable # 1 solution code
+class Car:
+    
+    # Deliverable # 1 solution code
+    def __init__(self, make, model, year, horn_volume=1):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.horn_volume = horn_volume
+
+    # Deliverable # 2 solution code
+    @property
+    def year(self):
+        return self._year
+    
+    # Deliverable # 2 solution code
+    @year.setter
+    def year(self, value):
+        if not (type(value) == int):
+            raise TypeError("Year must be an integer!")
+        elif not (1900 <= value <= 2024):
+            raise ValueError("Year must be between 1900 and 2024!")
+        else:
+            self._year = value
+
+    # Deliverable # 3 solution code
+    @property
+    def horn_volume(self):
+        return self._horn_volume
+    
+    # Deliverable # 3 solution code
+    @horn_volume.setter
+    def horn_volume(self, value):
+        if not (type(value) == int):
+            raise TypeError("Horn Volume must be an integer!")
+        elif not (1 <= value <= 10):
+            raise ValueError("Horn Volume must be between 1 and 10!")
+        else:
+            self._horn_volume = value
+
+    # Deliverable # 4 solution code
+    @property
+    def make(self):
+        return self._make
+    
+    # Deliverable # 4 solution code
+    @make.setter
+    def make(self, value):
+        if not (type(value) == str):
+            raise TypeError("Make must be a string!")
+        elif len(value) < 3:
+            raise ValueError("Make must be at least 3 characters long!")
+        else:
+            self._make = value
+
+    # Deliverable # 5 solution code
+    def honk_horn(self):
+        print(f"BEEP BEEP{'!' * self.horn_volume}")
